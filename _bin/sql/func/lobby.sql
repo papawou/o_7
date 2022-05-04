e8bb5a81004d7143086c5ad5463bca9338490cc8
Mendes <papawa.dev@gmail.com>
2022-05-04 19:46:21 +0200

    invitation + authz invit on lobby

--- _bin/sql/func/lobby.sql
+++ _bin/sql/func/lobby.sql
@@ -17,8 +17,31 @@ leave_lobby
     : lobby_member* SHARE
       lobby UPDATE
 */
-DROP FUNCTION IF EXISTS create_lobby, join_lobby, leave_lobby;
+DROP FUNCTION IF EXISTS raise_except;
+CREATE OR REPLACE FUNCTION raise_except(_trigger boolean, _msg text) RETURNS void AS $$
+BEGIN
+  IF _trigger THEN
+      RAISE EXCEPTION '%', _msg;
+  END IF;
+END
+$$ LANGUAGE plpgsql;
 
+DROP FUNCTION IF EXISTS lock_lobbyuser;
+CREATE OR REPLACE FUNCTION lock_lobbyuser(_shared boolean, _id_user integer) RETURNS void AS $$
+DECLARE
+  __hash bigint = hashtext('lobbyuser:'||_id_user);
+BEGIN
+  IF _shared THEN
+    PERFORM pg_advisory_xact_lock_shared(__hash);
+  ELSE
+    PERFORM pg_advisory_xact_lock(__hash);
+  END IF;
+  PERFORM raise_except(NOT FOUND, 'fail_lock lobbyuser');
+END
+$$ LANGUAGE plpgsql;
+
+
+DROP FUNCTION IF EXISTS create_lobby, join_lobby, leave_lobby;
 CREATE OR REPLACE FUNCTION create_lobby(_id_viewer integer, _max_slots integer, OUT id_lobby_ integer) AS $$
 BEGIN
   SET CONSTRAINTS fk_lobby_slots, fk_lobby_owner DEFERRED;
@@ -30,9 +53,13 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION join_lobby(_id_viewer integer, _id_lobby integer) RETURNS integer AS $$
 BEGIN
-  PERFORM FROM lobbys WHERE id = _id_lobby FOR SHARE; IF NOT FOUND THEN RAISE EXCEPTION 'lobby not found'; END IF;
+  PERFORM lock_lobbyuser(false, _id_viewer);
+  
+  PERFORM FROM lobbys WHERE id = _id_lobby FOR SHARE; PERFORM raise_except(NOT FOUND, 'lobby not found');
   UPDATE lobby_slots SET free_slots=free_slots-1 WHERE id_lobby = _id_lobby;
   INSERT INTO lobby_members(id_lobby, id_user) VALUES(_id_lobby, _id_viewer);
+  
+  DELETE FROM lobby_invitations WHERE id_lobby = _id_lobby AND id_target = _id_viewer;
   RETURN 1;
 END
 $$ LANGUAGE plpgsql;
@@ -49,17 +76,14 @@ DECLARE
   __slots integer;
 BEGIN
   SET CONSTRAINTS fk_lobby_owner DEFERRED;
-  SELECT id_lobby INTO __id_lobby FROM lobby_members WHERE id_user = _id_viewer; IF NOT FOUND THEN RAISE EXCEPTION 'viewer not in a lobby'; END IF;
+  SELECT id_lobby INTO __id_lobby FROM lobby_members WHERE id_user = _id_viewer; PERFORM raise_except(NOT FOUND, 'viewer not in a lobby');
   
-  --lock lobby authz
-  SELECT id_owner INTO __id_owner FROM lobbys WHERE id = __id_lobby FOR SHARE; IF NOT FOUND THEN RAISE EXCEPTION 'lobby not found'; END IF;
+  SELECT id_owner INTO __id_owner FROM lobbys WHERE id = __id_lobby FOR SHARE; PERFORM raise_except(NOT FOUND,'lobby not found');
   PERFORM FROM lobbys WHERE id = __id_lobby AND id_owner = _id_viewer FOR UPDATE;
   
   UPDATE lobby_slots SET free_slots = free_slots + 1 WHERE id_lobby = __id_lobby RETURNING max_slots - free_slots INTO __slots;
   
-  --lock lobby_member authz
-  DELETE FROM lobby_members WHERE id_lobby = __id_lobby AND id_user = _id_viewer; IF NOT FOUND THEN RAISE EXCEPTION 'viewer lobby_member no longer exist'; END IF;
-  
+  DELETE FROM lobby_members WHERE id_lobby = __id_lobby AND id_user = _id_viewer; PERFORM raise_except(NOT FOUND,'serialization error, viewer lobby_member no longer exist');
   IF __id_owner = _id_viewer THEN
     IF __slots = 0 THEN
       DELETE FROM lobbys WHERE id = __id_lobby;
@@ -75,5 +99,25 @@ BEGIN
     END IF;
   END IF;
   RETURN 1;
+END
+$$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS lobby_invite, lobby_invite_cancel;
+CREATE OR REPLACE FUNCTION lobby_invite(_id_viewer integer, _id_target integer, _id_lobby integer) RETURNS boolean AS $$
+DECLARE
+  __authz bool;
+BEGIN
+  PERFORM lock_lobbyuser(true, _id_target);
+  SELECT (id_owner = _id_viewer OR filter_join IS FALSE) INTO __authz FROM lobbys WHERE id = _id_lobby FOR SHARE; PERFORM raise_except(NOT __authz, 'lobby authz fail');
+  PERFORM FROM lobby_members WHERE id_lobby = _id_lobby AND id_user = _id_target; PERFORM raise_except(FOUND, 'target already member');
+  INSERT INTO lobby_invitations(id_lobby, id_target, id_creator) VALUES (_id_lobby, _id_target, _id_viewer);
+  RETURN FOUND;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION lobby_invite_cancel(_id_viewer integer, _id_target integer) RETURNS boolean AS $$
+BEGIN
+  DELETE FROM lobby_invitations WHERE id_creator = _id_viewer AND id_target = _id_target;
+  RETURN FOUND;
 END
 $$ LANGUAGE plpgsql;
--- _bin/sql/table/lobby.sql
+++ _bin/sql/table/lobby.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS lobbys, lobby_slots, lobby_members, lobby_invitations, lobb
 
 CREATE TABLE lobbys(
   id bigserial PRIMARY KEY,
-  id_owner integer REFERENCES users NOT NULL
+  id_owner integer REFERENCES users NOT NULL,
+  filter_join boolean NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE lobby_slots(
@@ -19,6 +20,28 @@ CREATE TABLE lobby_members(
   PRIMARY KEY(id_lobby, id_user)
 );
 ALTER TABLE lobbys ADD CONSTRAINT fk_lobby_owner FOREIGN KEY(id, id_owner) REFERENCES lobby_members DEFERRABLE;
+
+CREATE TABLE lobby_invitations(
+  id_lobby integer REFERENCES lobbys ON DELETE CASCADE,
+  id_target integer REFERENCES users,
+  id_creator integer REFERENCES users,
+  PRIMARY KEY(id_lobby, id_target, id_creator),
+  FOREIGN KEY (id_lobby, id_creator) REFERENCES lobby_members ON DELETE CASCADE
+);
+
+DROP TYPE lobby_request_status;
+CREATE TYPE lobby_request_status AS ENUM ('wait_lobby', 'wait_user', 'inv_wait_lobby');
+CREATE TABLE lobby_requests(
+  id_lobby integer REFERENCES lobbys ON DELETE CASCADE,
+  id_user integer REFERENCES users,
+  PRIMARY KEY(id_lobby, id_user),
+  status lobby_request_status NOT NULL DEFAULT 'wait_lobby',
+  id_creator integer REFERENCES users,
+  FOREIGN KEY(id_lobby, id_creator) REFERENCES lobby_members,
+  FOREIGN KEY(id_lobby, id_user, id_creator) REFERENCES lobby_invitations,
+  CHECK (id_creator IS NULL AND (status = 'wait_lobby') OR (id_creator IS NOT NULL AND status  ='inv_wait_lobby'))
+);
+
 
 /*
 CREATE TABLE lobby_bans(
